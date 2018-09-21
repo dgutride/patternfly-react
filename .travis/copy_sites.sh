c5d8578b25912f7fced4c60a9e0f0efe74620e10
Dana Gutride <dgutride@redhat.com>
2018-09-21 09:50:01 -0400

    fix(public): add public folder to repo

--- .travis/copy_sites.sh
+++ .travis/copy_sites.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cp -r .out/ public/patternfly-3
-cp -r packages/patternfly-4/react-docs/public public/patternfly-4
+cp -r .out/ .public/patternfly-3
+cp -r packages/patternfly-4/react-docs/public .public/patternfly-4
